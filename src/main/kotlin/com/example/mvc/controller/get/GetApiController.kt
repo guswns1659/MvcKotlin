d14c3c8340ba95a2b3304ffcef404c421a1f44f8
hyunjun1659 <jack@rainist.com>
2020-11-14 15:14:29 +0900

    path-variable 테스트 성공

--- src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
+++ src/main/kotlin/com/example/mvc/controller/get/GetApiController.kt
@@ -1,15 +1,25 @@
 package com.example.mvc.controller.get
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController // Rest API Controller 동작
 @RequestMapping("/api")
 class GetApiController {
 
-    @GetMapping("/hello")
+    @GetMapping(path = ["/hello", "/abcd"])
     fun hello(): String{
         return "hello kotlin"
+    }
+
+    // http 제약없이 사용
+    @RequestMapping(method = [RequestMethod.GET], path =  ["/request-mapping"])
+    fun requestMapping(): String {
+        return "request-mapping"
+    }
+
+    @GetMapping("/get-mapping/path-variable/{name}/{age}")
+    fun pathVariable(@PathVariable name: String, @PathVariable age:Int): String {
+        println("$name, $age")
+        return "$name $age"
     }
 }
--- src/test/kotlin/com/example/mvc/controller/get/GetApiControllerTest.kt
+++ src/test/kotlin/com/example/mvc/controller/get/GetApiControllerTest.kt
@@ -1,7 +1,10 @@
 package com.example.mvc.controller.get
 
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
@@ -9,6 +12,8 @@ import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.bind.annotation.PathVariable
+import kotlin.system.exitProcess
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "30000")
@@ -44,4 +49,23 @@ class GetApiControllerTest {
                 }
     }
 
+    @DisplayName("path-variable 이용한 GetMapping 테스트")
+    @ParameterizedTest
+    @CsvSource("jack,10,jack 10")
+    fun getPathVariable(name: String, age: Int, expected: String) {
+        // given
+        val requestUrl: String = LOCALHOST + port + REQUEST_MAPPING +
+                "get-mapping/path-variable/" + name + "/" + age
+
+        // when
+        webTestClient.get()
+                .uri(requestUrl)
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody()
+                .consumeWith {
+                    assertThat(it.responseBody?.let {it1 -> String(it1)}).isEqualTo(expected)
+                }
+    }
 }
