f40c1b0ac7daed7a82513ff8cadf85d7ef55b5b9
guswns1659 <guswns1659@gmail.com>
2020-12-09 20:28:55 +0900

    Custom Validation Annotation 만들어서 사용하기

--- src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/mvc/model/http/UserRequest.kt
@@ -1,27 +1,18 @@
 package com.example.mvc.model.http
 
+import com.example.mvc.annotation.StringFormatDateTime
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import javax.validation.constraints.AssertTrue
 
-data class UserRequest (
+data class UserRequest(
 
-        var name: String?=null,
-        var phoneNumber: String?=null,
+    var name: String? = null,
+    var phoneNumber: String? = null,
 
-        var createdAt:String?=null // yyyy-MM-dd HH:mm:ss
-) {
-    @AssertTrue(message = "생성일자의 패턴은 yyyy-MM-dd HH:mm:ss")
-    private fun isValidCreatedAt():Boolean {
+    @field:StringFormatDateTime(pattern = "yyyy-MM-dd HH:mm:ss", message = "패턴이 올바르지 않습니다.")
+    var createdAt: String? = null // yyyy-MM-dd HH:mm:ss
+)
 
-        try {
-            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-            return true
-
-        } catch (e:Exception) {
-            return false
-        }
-    }
-}
 
---
+++ src/main/kotlin/com/example/mvc/validater/StringFormatDateTimeValidator.kt
@@ -0,0 +1,27 @@
+package com.example.mvc.validater
+
+import com.example.mvc.annotation.StringFormatDateTime
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import javax.validation.ConstraintValidator
+import javax.validation.ConstraintValidatorContext
+
+class StringFormatDateTimeValidator : ConstraintValidator<StringFormatDateTime, String> {
+
+    private var pattern : String? = null
+
+    override fun initialize(constraintAnnotation: StringFormatDateTime?) {
+        this.pattern = constraintAnnotation?.pattern
+    }
+
+    // 정상이면 true, 비정상이면 false
+    override fun isValid(value: String?, context: ConstraintValidatorContext?): Boolean {
+        try {
+            LocalDateTime.parse(value, DateTimeFormatter.ofPattern(pattern))
+            return true
+
+        } catch (e:Exception) {
+            return false
+        }
+    }
+}
